923109bd15ef04e75fcc75db4c843df72f1d67e6
Joel Angelo R. Sudiacal <jsudiacal@exist.com>
2016-04-20 14:56:04 +0800

    initial

---
+++ files_files/en.js
@@ -0,0 +1,11 @@
+//Google Custom Search Engine
+(function() {
+  var cx = '012029519579280034868:3hzywhss5ik';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+      '//www.google.com/cse/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
---
+++ files_files/search.js
@@ -0,0 +1,1 @@
+google.sbox.p50 && google.sbox.p50(["a",[],{"j":"o","k":1,"q":"cXpd9dwtHhFcfWvOq_AyM3EKURE"}])
